3c10d49cc63b4cceb66e1d2b4347205acf1d80bb
Jones <CJ304345@davenportschools.org>
2019-05-01 12:58:30 -0500

    database

--- src/Database.java
+++ src/Database.java
@@ -1,4 +1,15 @@
-public class Database
-{
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
+public class Database {
+    public static void saveWords(String args){
+        try {
+            PrintWriter printWriter = new PrintWriter("Data.txt", "UTF-8");
+            printWriter.println(args);
+            printWriter.close();
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
 }
